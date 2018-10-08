5583ee8df961417959469d2f8106c470f1eb9034
Paul Müller <dev@craban.de>
2018-10-08 10:28:43 +0200

    ci: path

--- .travis/osx_install_python.sh
+++ .travis/osx_install_python.sh
@@ -25,11 +25,13 @@ PKG="python-${MPV}-macosx10.6.pkg"
 wget -c -P ${DLD} https://www.python.org/ftp/python/${MPV}/${PKG}
 
 # install MacPython
-sudo installer -pkg ${DLD}/${PKG} -target /
+installer -pkg ${DLD}/${PKG} -target /
+
+export PATH="/Library/Frameworks/Python.framework:$PATH"
 
 # install latest version of pip
 # (avoids [SSL: TLSV1_ALERT_PROTOCOL_VERSION] errors)
 curl https://bootstrap.pypa.io/get-pip.py -o ${DLD}/get-pip.py
-sudo python ${DLD}/get-pip.py
+python ${DLD}/get-pip.py
 
 cd $OLD
