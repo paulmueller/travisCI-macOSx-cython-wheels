ccb8c5c91fd8b8172caf1c19e18cbf4ef5a7fdd3
Paul Müller <dev@craban.de>
2018-10-08 10:23:19 +0200

    ci: updaet

--- .travis/osx_install_python.sh
+++ .travis/osx_install_python.sh
@@ -30,6 +30,6 @@ sudo installer -pkg ${DLD}/${PKG} -target /
 # install latest version of pip
 # (avoids [SSL: TLSV1_ALERT_PROTOCOL_VERSION] errors)
 curl https://bootstrap.pypa.io/get-pip.py -o ${DLD}/get-pip.py
-python ${DLD}/get-pip.py
+sudo python ${DLD}/get-pip.py
 
 cd $OLD
