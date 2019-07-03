6a31e3fd6cecc3f99d739c3a04e8874d20c3ca3c
Paul Müller <dev@craban.de>
2019-07-03 21:34:49 +0200

    drop support for python 2, add pyproject.toml

--- README.rst
+++ README.rst
@@ -32,7 +32,7 @@ Notes:
 - All relevant scripts are located in the ``.travis`` directory.
 - The version of the ``foo`` package is determined from the current git tag and
   wheels are named accordingly.
-- Tested on osx 10.10.5 with the native Python2.7 installation.
+- Tested on osx 10.10.5
 
 .. |Build Status| image:: https://img.shields.io/travis/paulmueller/travisCI-macOSx-cython-wheels.svg
    :target: https://travis-ci.org/paulmueller/travisCI-macOSx-cython-wheels/
--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 from os.path import exists, dirname, realpath
 from setuptools import setup, Extension, find_packages
 import sys
 
-author = u"Paul Müller"
+# numpy and cython are installed via pyproject.toml [build-system]
+import numpy as np
+
+author = "Paul Müller"
 authors = [author]
 description = 'Example library for building wheels for macOSx on travisCI'
 name = 'foo'
@@ -19,6 +20,7 @@ except:
 
 extensions = [Extension("foo.bar",
                         sources=["foo/bar.pyx"],
+                        include_dirs=[np.get_include()],
                         )
               ]
 
@@ -37,7 +39,8 @@ setup(
     long_description=open('README.rst').read() if exists('README.rst') else '',
     ext_modules = extensions,
     keywords=["foo-bar"],
-    setup_requires=['cython', 'pytest-runner'],
+    setup_requires=["pytest-runner"],
     tests_require=["pytest"],
+    python_requires='>=3.2, <4',
     platforms=['ALL'],
     )
